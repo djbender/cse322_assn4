642cfa5694b40d2157f4e4afe6161b5151236828
Derek Bender <nofeardjb@gmail.com>
2010-12-01 15:44:24 -0600

    wed, work

--- test.pl
+++ test.pl
@@ -1,13 +1,18 @@
 noRepairCoverage(Fuel, Region, Cell) :-
 	process(Region, Cell).
 
+
 process([],_,_,L,L).
 process([[]|Rest],_,Y,L,Z) :-
 	X1 is 0,
 	Y1 is Y+1,
 	process(Rest,X1,Y1,L,Z).
+	
 process([[Head|Tail]|Rest],X,Y,L,Z) :-
-	write('@'), write(X), write(','), write(Y), write(': '), write(Head), nl, 
+	/*DEBUG*/ write('@'), write(X), write(','), write(Y), write(': '), write(Head), nl, 
+	(Head = 2 -> write('Station Found!'); ), nl,
 	X1 is X+1,
 	Count is L+1, 
 	process([Tail|Rest],X1,Y,Count,Z).
+
+/*findTrack()*/
