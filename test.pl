c17f5f9d844f05a333ca8ea436a75675f622a77f
Derek Bender <nofeardjb@gmail.com>
2010-12-01 08:49:32 -0600

    test.pl can now process a grid

--- test.pl
+++ test.pl
@@ -1,8 +1,13 @@
 noRepairCoverage(Fuel, Region, Cell) :-
 	process(Region, Cell).
 
-process([],A,A).
-process([[]|Z],A,B) :-
-	process(Z,A,B).
-process([[H|T]|Z],A,B) :-
-	write(H), nl, C is A+1, process([T|Z],C,B).
+process([],_,_,L,L).
+process([[]|Rest],_,Y,L,Z) :-
+	X1 is 0,
+	Y1 is Y+1,
+	process(Rest,X1,Y1,L,Z).
+process([[Head|Tail]|Rest],X,Y,L,Z) :-
+	write('@'), write(X), write(','), write(Y), write(': '), write(Head), nl, 
+	X1 is X+1,
+	Count is L+1, 
+	process([Tail|Rest],X1,Y,Count,Z).
